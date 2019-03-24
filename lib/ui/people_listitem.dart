e69b767595d01431439202d368a8dfdb1784c62b
Exequiel Beker <ezzeyako@gmail.com>
2019-03-24 17:08:43 -0300

    Implemented People as list item.

---
+++ lib/ui/people_listitem.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+
+import '../models/people_model.dart';
+
+class PeopleListItem extends StatelessWidget {
+  PeopleModel _people;
+
+  PeopleListItem(PeopleModel model) {
+    _people = model;
+  }
+
+  Widget _imageHeader(BuildContext context) {
+    return new CircleAvatar(
+      backgroundImage: Image.network(
+              "https://previews.123rf.com/images/dmstudio/dmstudio1005/dmstudio100500024/7013643-br%C3%BAjula-de-vector-de-grunge-.jpg")
+          .image,
+      child: Text(_people.Name[0] + _people.Surname[0]),
+    );
+  }
+
+  Widget _dataCenter(BuildContext context) {
+    return new Expanded(
+        child: Container(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        new Text(
+          _people.Name + " " + _people.Surname,
+          style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+        ),
+        new Text("Hello from Kairos as ListItem!")
+      ],
+    )));
+  }
+
+  Widget _buttons(BuildContext context) {
+    return new Column(
+      mainAxisAlignment: MainAxisAlignment.end,
+      mainAxisSize: MainAxisSize.min,
+      crossAxisAlignment: CrossAxisAlignment.end,
+      children: <Widget>[
+        IconButton(
+          icon: Icon(Icons.plus_one),
+        )
+      ],
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return new Container(
+        decoration: BoxDecoration(
+            border: Border.all(color: Colors.black26, width: 0.7),
+            borderRadius: BorderRadius.all(Radius.circular(8.0))),
+        padding: EdgeInsets.all(2.0),
+        margin: EdgeInsets.all(8.0),
+        child: Row(
+          mainAxisSize: MainAxisSize.max,
+          children: <Widget>[
+            _imageHeader(context),
+            _dataCenter(context),
+            _buttons(context),
+          ],
+        ));
+  }
+}
--- lib/ui/people_ui.dart
+++ lib/ui/people_ui.dart
@@ -5,6 +5,7 @@ import '../models/people_model.dart';
 
 /* Other related UIs */
 import 'people_card.dart';
+import 'people_listitem.dart';
 
 class PeopleListUI extends StatefulWidget {
   @override
@@ -14,50 +15,10 @@ class PeopleListUI extends StatefulWidget {
 }
 
 class PeopleUI extends State<PeopleListUI> {
-/*  Widget _buildItemAsCard(
-      AsyncSnapshot<List<PeopleModel>> snapshot, int index) {
-    return new Card(
-      child: Column(children: <Widget>[
-        Row(
-          children: <Widget>[
-            CircleAvatar(
-              backgroundColor: Colors.deepPurple,
-              backgroundImage: (snapshot.data.elementAt(index).Image != null
-                  ? (new Image.memory(
-                      snapshot.data.elementAt(index).Image.getBytes()))
-                  : new Image.network(
-                          "https://www.andiar.com/4186-thickbox_default/vinilo-brujula.jpg")
-                      .image),
-            ),
-            new Text(
-              snapshot.data.elementAt(index).Name +
-                  " " +
-                  snapshot.data.elementAt(index).Surname,
-              textAlign: TextAlign.center,
-              style: new TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
-            ),
-          ],
-        ),
-        Row(
-          children: <Widget>[
-            new Expanded(child: new Text(" Actually, no data. Sorry!"))
-          ],
-        ),
-        Expanded(
-            child: Align(
-                child: Row(
-          children: <Widget>[
-            new IconButton(
-              icon: new Icon(Icons.grade),
-              onPressed: null,
-              alignment: FractionalOffset.bottomRight,
-            )
-          ],
-        )))
-      ]),
-    );
-  }
-*/
+
+  /* TODO Mocked value */
+  final bool _viewAsList = true;
+
   @override
   Widget build(BuildContext context) {
     bloc.fetchAllPeople();
@@ -83,14 +44,22 @@ class PeopleUI extends State<PeopleListUI> {
 
   /* TODO build as truly list */
   Widget buildItems(AsyncSnapshot<List<PeopleModel>> snapshot) {
-    return new GridView.builder(
-      itemCount: snapshot.data.length,
-      gridDelegate:
-          new SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
-      itemBuilder: (BuildContext context, int index) {
-        /* TODO Card layout */
-        return PeopleCard(snapshot.data[index]);
+    if(_viewAsList == true) {
+      return new ListView.builder(itemBuilder: (BuildContext context, int index) {
+        return PeopleListItem(snapshot.data[index]);
       },
-    );
+      itemCount: snapshot.data.length,
+      );
+    } else {
+      return new GridView.builder(
+        itemCount: snapshot.data.length,
+        gridDelegate:
+        new SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
+        itemBuilder: (BuildContext context, int index) {
+          /* TODO Card layout */
+          return PeopleCard(snapshot.data[index]);
+        },
+      );
+    }
   }
 }
