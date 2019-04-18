e4dc56a6354868ff9c53a7dd3d83884e4cc63eab
Exequiel Beker <exequielbeker@gmail.com>
2019-04-18 13:07:36 -0300

    - Removed list pseudo cache from BLoC.

--- lib/blocs/PeopleBLoC.dart
+++ lib/blocs/PeopleBLoC.dart
@@ -6,9 +6,6 @@ import '../resources/PeopleRepository.dart' show PeopleRepository;
 final PeopleBloc peopleBloc = PeopleBloc();
 
 class PeopleBloc {
-  /* People cache */
-  List<People> _people = new List<People>();
-
   /* Repository, handling internally all data from services (web/local) */
   final _repository = PeopleRepository();
 
@@ -37,34 +34,23 @@ class PeopleBloc {
 
   /* Private controller listener. All magic is done here */
   void _handleRemovePeople(People item) async {
-    _repository.deletePeople(item);
-    _people.remove(item);
-    _notify();
+    _repository.deletePeople(item).then((value) {_notify();});
   }
 
   void _handleAddPeople(People item) async {
-    final newPerson = await _repository.addPeople(item.name, item.surname, item.shortBio, item.sectionID);
-
-    /* Add to people list, based on item returned by repository's add people call. */
-    _people.add(newPerson);
-    _notify();
+    _repository.addPeople(item.name, item.surname, item.shortBio, item.sectionID).then((value) {_notify();});
   }
 
   void _handleUpdatePeople(People item) async {
-	  await _repository.updatePeople(item);
-
-	  _people.clear();
-	  _people.addAll(await _repository.fetchAllPeople());
-	  _notify();
+    _repository.updatePeople(item).then((value) => _notify());
   }
 
   void _notify() async {
-    _inPeopleSink.add(_people);
+    _inPeopleSink.add(await _repository.fetchAllPeople());
   }
 
   /* PUBLIC METHODS */
   dispose() async {
-    _people.clear();
     await _peopleFetchController?.drain()?.whenComplete(() => _peopleFetchController?.close());
     await _peopleAddController?.drain()?.whenComplete(() => _peopleAddController?.close());
     await _peopleRemoveController?.drain()?.whenComplete(() => _peopleRemoveController?.close());
@@ -72,9 +58,7 @@ class PeopleBloc {
   }
 
   fetchAllPeople() async {
-    _people.clear();
-    _people.addAll(await _repository.fetchAllPeople());
-    _notify();
+    _repository.fetchAllPeople().then((value) {_notify();});
   }
 
   submitNewPeople(String name, String surname, String shortBio, int sectionID) async {
